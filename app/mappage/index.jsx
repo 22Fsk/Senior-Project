fb9740e91fe5feb93877c8a9d5f822182753d33d
FH2344 <fa6oom003@gmail.com>
2025-02-11 12:08:21 +0300

    btmsheet search css

--- app/mappage/index.jsx
+++ app/mappage/index.jsx
@@ -1,12 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View, TextInput } from 'react-native';
 import React, { useCallback, useRef, useState } from 'react';
 import BottomSheet, { BottomSheetView} from '@gorhom/bottom-sheet';
-import { GestureHandlerRootView } from 'react-native-gesture-handler'; // Import GestureHandlerRootView
+import { GestureHandlerRootView, ScrollView } from 'react-native-gesture-handler'; // Import GestureHandlerRootView
+import { Feather, FontAwesome6, Ionicons } from '@expo/vector-icons';
 
 const Index = () => {
   const sheetRef = useRef(null); // Make sure the ref is typed correctly if using TypeScript
   const [isOpen, setIsOpen] = useState(true);
-  const snapPoints = ['25%', '50%','70%'];
+  const snapPoints = ['30%', '50%','95%'];
+  const [serach, setSearch] = useState('');
+  const searchInputRef = useRef(null);
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}> {/* Wrap your component in GestureHandlerRootView */}
@@ -16,7 +19,29 @@ const Index = () => {
         
       >
         <BottomSheetView style={styles.btm}> {/* Wrap the content inside BottomSheetView */}
-          
+          <ScrollView contentContainerStyle={{gap: 15}}>
+            {/** search bar */}
+        <View style={styles.searchBar}>
+            <View style={styles.searchIcon}>
+                <Feather name="search" size={20} color={'gray'} />
+            </View>
+            <TextInput 
+            placeholder='Search for places...' 
+            value={serach}
+            ref={searchInputRef}
+            onChangeText={value=> setSearch(value)}
+            style={styles.searchInput} />
+            {
+                serach && (
+                    <Pressable style={styles.closeIcon}>
+                        <Ionicons name="close" size={20} color={'black'}/>
+                    </Pressable>  
+                )
+            }
+
+            
+        </View>
+          </ScrollView>
         </BottomSheetView>
       </BottomSheet>
     </GestureHandlerRootView>
@@ -33,6 +58,33 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   btm: {
-    padding: 50, 
-  }
+    padding: 20, 
+  },
+  searchBar: {
+    marginHorizontal: 0,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'gray',
+    backgroundColor: 'white',
+    padding: 4,
+    paddingLeft: 10,
+    borderRadius: 30,
+},
+searchIcon: {
+    padding: 8,
+},
+searchInput: {
+    flex: 1,
+    borderRadius: 10,
+    paddingVertical: 10,
+    fontSize: 15,
+},
+closeIcon:{
+    backgroundColor: 'lightgray',
+    padding:1,
+    borderRadius: 20,
+    marginRight:6,
+}
 });
